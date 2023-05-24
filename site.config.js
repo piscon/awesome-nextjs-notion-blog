9e0cac2d694aa98f25093bf7c8287a2daeedd566
b450 <b450@piscon.com>
2023-05-24 13:58:29 +0900

    Change Home Page

--- site.config.js
+++ site.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '6a76fdd9ec6d4ae2855aa790d620eec4',
+  rootNotionPageId: 'e78bc0de72e8489e9591c26236e3b658',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
